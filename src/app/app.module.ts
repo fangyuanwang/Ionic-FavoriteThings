4757bf5d0d6d7c0426a556b9234129eb7f242a29
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-07-31 21:20:45 -0400

    save color to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7BOPcToAT9R7TDyr4_wnJE615l2hpybk",
+  authDomain: "ionic-favorite-things-2be28.firebaseapp.com",
+  databaseURL: "https://ionic-favorite-things-2be28.firebaseio.com",
+  projectId: "ionic-favorite-things-2be28",
+  storageBucket: "ionic-favorite-things-2be28.appspot.com",
+  messagingSenderId: "149017665195"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +24,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from "angularfire2/database";
+
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  favoriteColor: string;
 
+  constructor(public navCtrl: NavController,
+    private db: AngularFireDatabase) {
+
+  }
+
+  ngOnInit(): void {
+    firebase.database().ref('color').on("value", (snapShot: firebase.database.DataSnapshot) => { 
+      this.favoriteColor = snapShot.val();
+    });
   }
 
   setColor(selectedColor: string): void {
     console.log(selectedColor);
+    firebase.database().ref("/color").set(selectedColor);
   }
+
+  
 
 }
